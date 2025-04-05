1f7ea026928a7f722c55a4080c7f8fd4d54b506b
Simpa0104 <Steventorres042001@gmail.com>
2025-04-05 12:49:23 -0500

    Se le hizo la simulacion del cuestionario

--- pruebas/Pruebask6/concurrencia_test.js
+++ pruebas/Pruebask6/concurrencia_test.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 export let options = {
-    vus: 50,
-    duration: '30s',
+    vus: 1000,
+    duration: '1200s',
 };
 
 export default function () {
--- pruebas/Pruebask6/rendimiento_test.js
+++ pruebas/Pruebask6/rendimiento_test.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 50,
-  duration: '30s',
+  vus: 100,
+  duration: '60s',
 };
 
 export default function () {
